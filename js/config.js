071ffc6fb1af13a19f750c8dcaa0a3e795e55d44
smartchain11 <smartchain11@users.noreply.github.com>
2026-09-16 10:41:32 +0800

    Update Supabase credentials

--- js/config.js
+++ js/config.js
@@ -4,8 +4,8 @@
 // credentials found in: Project Settings → API
 // ============================================================
 
-const SUPABASE_URL = "https://xbngjebfemyjminokllk.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_tWEry3NJKmK_2spHZWQ73g_ZncKnqno";
+const SUPABASE_URL = "https://goghypbfnxochcuftjne.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_wizUH1y0NTnU3do1UBbPlg_9je7Nzxy";
 
 // Do not modify below this line
 const SUPABASE = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
